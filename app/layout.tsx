7eed30e411a6cc863c2a3fb36fcc39991d082dbc
TzuHan_Chen <hahachentzuhan@gmail.com>
2025-08-09 23:37:02 +0800

    feat: 加入舊版程式碼

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,13 @@ export const metadata: Metadata = {
   description: "可以穿梭時空的井字遊戲",
 };
 
-export default function RootLayout({ children,
-}: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`bg-gray-100 dark:bg-gray-950 ${notoSansTC.variable} antialiased font-noto-sans-tc text-gray-800 dark:text-gray-300 selection:bg-teal-700 selection:text-teal-100`}>
-        {children}
+      <body className={`bg-gray-100 ${notoSansTC.variable} antialiased font-noto-sans-tc text-gray-800 selection:bg-teal-700 selection:text-teal-100`}>
+        <main className="min-h-screen p-6 flex flex-col justify-center items-center">
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,147 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+function calculateWinner(squares: any) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function calculateDraw(currentMove: number) {
+  if (currentMove === 9) {
+    return true;
+  }
+  return false;
+}
+
+function Square({ value, onSquareClick }:
+  { value: any, onSquareClick: any }) {
+  return (
+    <button className="-mr-1 -mt-1 size-9 p-0 border border-gray-200 bg-white text-teal-700 text-2xl font-bold leading-9 text-center hover:cursor-pointer active:cursor-pointer" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }:
+  { xIsNext: any, squares: any, onPlay: any }) {
+  function handleClick(i: number) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+
+  let threeSquare = [], threeRow = [];
+  for (let i = 0; i < 3; i++) {
+    threeSquare = [];
+    for (let j = 0; j < 3; j++) {
+      threeSquare.push(
+        <Square value={squares[i * 3 + j]}
+          onSquareClick={() => handleClick(i * 3 + j)}
+          key={j} />
+      );
+    }
+    threeRow.push(
+      <div className="-mb-1 h-9 flex items-start" key={i}>
+        {threeSquare}
+      </div>
+    );
+  }
+  return <div className="w-27 mx-auto">{threeRow}</div>;
+}
+
+function Status({ xIsNext, squares, currentMove }:
+  { xIsNext: boolean, squares: any, currentMove: number }) {
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = '獲勝者: ' + winner;
+  } else if (calculateDraw(currentMove)) {
+    status = '平手';
+  } else {
+    status = '下一位: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return <h2 className="mt-1.5 text-2xl font-bold text-center">{status}</h2>;
+}
+
+function History({ history, currentMove, setCurrentMove }:
+  { history: any[][], currentMove: number, setCurrentMove: any }) {
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move === currentMove) {
+      description =
+        <span className="leading-9 font-bold text-teal-700">
+          你在第 {move} 回合
+        </span>;
+    } else if (move === 0) {
+      description =
+        <button className="" onClick={() => jumpTo(move)}>
+          前往遊戲的起點
+        </button>;
+    } else {
+      description =
+        <button className="rounded-2xl border-none h-7.5 py-1.5 px-3 bg-gray-200 hover:cursor-pointer hover:bg-teal-700 hover:text-white active:cursor-pointer active:bg-teal-700 active:text-white transition-colors duration-300" onClick={() => jumpTo(move)}>
+          前往第 {move} 回合
+        </button>;
+    }
+    return (
+      <li className="h-9 list-none text-center" key={move}>{description}</li>
+    );
+  });
+
+  return <ol>{moves}</ol>;
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [currentMove, setCurrentMove] = useState(0);
+	const xIsNext = currentMove % 2 === 0;
+	const currentSquares = history[currentMove];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+	function handlePlay(nextSquares: any) {
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
+	}
+
+  return (
+    <div className="shadow-md rounded-2xl w-75 min-h-108 bg-gray-100 px-3 text-gray-800">
+      <h1 className="my-1.5 text-4xl font-bold text-center">井字遊戲</h1>
+      <div className="flex justify-between">
+        <div className="w-1/2">
+          <Board xIsNext={xIsNext}
+            squares={currentSquares}
+            onPlay={handlePlay} />
+          <Status xIsNext={xIsNext}
+            squares={currentSquares}
+            currentMove={currentMove} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="w-1/2">
+          <History history={history} currentMove={currentMove} setCurrentMove={setCurrentMove} />
+        </div>
+      </div>
     </div>
   );
 }
